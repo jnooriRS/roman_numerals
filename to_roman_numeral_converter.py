83857a05be88b3c0ec2c5224d3bd1abd355e8215
jnooriRS <jonas.noori@riversafe.co.uk>
2022-09-06 12:29:43 +0100

    basic converter working

--- to_roman_numeral_converter.py
+++ to_roman_numeral_converter.py
@@ -1,15 +1,17 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        result = 0
+def romanToInt(s: str) -> int:
+    roman_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
 
-        for numeral in range(len(s)):
-            if numeral + 1 < len(s) and roman_table[numeral] < roman_table[numeral + 1]:
-                result -= roman_table[numeral]
-            else:
-                result += roman_table[numeral]
+    for numeral in range(len(s)):
+        if (
+            numeral + 1 < len(s)
+            and roman_table[s[numeral]] < roman_table[s[numeral + 1]]
+        ):
+            result -= roman_table[s[numeral]]
+        else:
+            result += roman_table[s[numeral]]
 
-        return result
+    return result
 
 
-Solution("MCMXCIV")
+print(romanToInt("MCMXCIV"))
